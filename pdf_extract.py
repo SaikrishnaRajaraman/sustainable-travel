056ec84908c4aab9542639368b7e26793421c7d7
Saikrishna Rajaraman <saikrishnarajaraman@Saikrishnas-MacBook-Pro.local>
2024-11-05 21:01:44 -0500

    Air travel emissions updated

--- pdf_extract.py
+++ pdf_extract.py
@@ -2,7 +2,7 @@ import pymupdf  # PyMuPDF
 import airportsdata
 import re
 import requests
-from utils.utils import ground_carbon_emission
+from utils.utils import ground_carbon_emission,calculate_flight_emissions
 from collections import defaultdict
 import csv
 import os
@@ -13,8 +13,7 @@ import csv
 from openai import OpenAI
 import json
 from dotenv import load_dotenv
-from clean_data import get_airports_from_data
-from compute_air_emissions import get_airport_pairs
+from compute_air_emissions import get_airports_from_athletics_data,get_airports_data_from_works_report
 
 #Load the airports data using IATA code
 airports = airportsdata.load('IATA')
@@ -27,58 +26,59 @@ def isAirport(airport_code):
     airport_codes = [code for code in airport_code if code in airports]
     return airport_codes
 
-def getPDFfromURL():
+# def getPDFfromURL():
 
-    login_payload = {
-        'username': 'dummy',
-        'password': 'dummy'
-    }
-    URL = ''
+#     login_payload = {
+#         'username': 'dummy',
+#         'password': 'dummy'
+#     }
+#     URL = ''
 
-    session = requests.Session()
+#     session = requests.Session()
 
-    login_response = session.post(URL, data=login_payload)
+#     login_response = session.post(URL, data=login_payload)
 
-    if login_response.status_code == 200:
-        print("Logged in successfully!")
+#     if login_response.status_code == 200:
+#         print("Logged in successfully!")
 
-        response = session.get(URL, stream=True)
+#         response = session.get(URL, stream=True)
 
-        # Check if the request was successful
-        if response.status_code == 200:
-            # Print the content of the page (HTML source)
-            print(response.text)
-        else:
-            print(f"Failed to access the page. Status code: {response.status_code}")
+#         # Check if the request was successful
+#         if response.status_code == 200:
+#             # Print the content of the page (HTML source)
+#             print(response.text)
+#         else:
+#             print(f"Failed to access the page. Status code: {response.status_code}")
 
-        # content_type = response.headers.get('Content-Type', '')
-        # if 'application/pdf' in content_type:
-        #     print("PDF found")
-        # else: 
-        #     print()
-    return
-    # Raise an exception for HTTP errors (4xx or 5xx)
-    response.raise_for_status()
+#         # content_type = response.headers.get('Content-Type', '')
+#         # if 'application/pdf' in content_type:
+#         #     print("PDF found")
+#         # else: 
+#         #     print()
+#     return
+#     # Raise an exception for HTTP errors (4xx or 5xx)
+#     response.raise_for_status()
 
-    content_type = response.headers.get('Content-Type', '')
+#     content_type = response.headers.get('Content-Type', '')
 
-    if 'application/pdf' in content_type:
-        print("PDF found")
-    return
-    # Check if the response contains a PDF
-    content_type = response.headers.get('Content-Type', '')
-    response = requests.get(URL)
-    if response.status_code == 200:
-        with open('downloaded_pdf.pdf', 'wb') as f:
-            for chunk in response.iter_content(chunk_size=1024):
-                    if chunk:  # Filter out keep-alive new chunks
-                        print(chunk)
-                        f.write(chunk)
-        print("PDF downloaded successfully")
-    else:
-        print(f"Failed to download PDF. Status code: {response.status_code}")
+#     if 'application/pdf' in content_type:
+#         print("PDF found")
+#     return
+#     # Check if the response contains a PDF
+#     content_type = response.headers.get('Content-Type', '')
+#     response = requests.get(URL)
+#     if response.status_code == 200:
+#         with open('downloaded_pdf.pdf', 'wb') as f:
+#             for chunk in response.iter_content(chunk_size=1024):
+#                     if chunk:  # Filter out keep-alive new chunks
+#                         print(chunk)
+#                         f.write(chunk)
+#         print("PDF downloaded successfully")
+#     else:
+#         print(f"Failed to download PDF. Status code: {response.status_code}")
 
 def load_emission_data_from_csv(csv_file):
+    emissions_data = defaultdict(EmissionModel)
     if os.path.exists(csv_file):
         with open(csv_file, mode='r') as file:
             reader = csv.DictReader(file)
@@ -92,43 +92,45 @@ def load_emission_data_from_csv(csv_file):
                 miles = row['Miles']
                 carbon_emission = row['Carbon Emission']
 
-                existing_airports[(source, destination)] = EmissionModel(source, destination, source_lat, destination_lat, source_long, destination_long, miles, carbon_emission)
+                emissions_data[(source, destination)] = EmissionModel(source, destination, source_lat, destination_lat, source_long, destination_long, miles, carbon_emission)
 
     else:
         print(f"File {csv_file} does not exist")
 
-def send_to_LLM(lines):
-    openai_api_key = os.getenv("OPENAI_API_KEY")
-    client = OpenAI(api_key = openai_api_key)
+    return emissions_data   
+
+# def send_to_LLM(lines):
+#     openai_api_key = os.getenv("OPENAI_API_KEY")
+#     client = OpenAI(api_key = openai_api_key)
     
-    content = '''
-    Extract all the airport legs for the travel based on the given content and return it in this required format?
-    [{
-        "from_airport": "IATA Code",
-        "to_airport": "IATA Code"
-    }]
-    '''
-    try:
-        response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[
-                {"role": "system", "content": ".".join(lines)},
-                {"role": "user", "content": content}
-            ],
-            max_tokens=150,
-            )
-        output = response.choices[0].message.content
-        output_lines = output.splitlines()
-        output_lines = output_lines[1:-1]
-        output = "\n".join(output_lines)
-        # print(output)
-        flight_data = json.loads(output)
-        return flight_data
-    except Exception as e:
-        print(f"An error occurred: {e}")
+#     content = '''
+#     Extract all the airport legs for the travel based on the given content and return it in this required format?
+#     [{
+#         "from_airport": "IATA Code",
+#         "to_airport": "IATA Code"
+#     }]
+#     '''
+#     try:
+#         response = client.chat.completions.create(
+#             model="gpt-4o-mini",
+#             messages=[
+#                 {"role": "system", "content": ".".join(lines)},
+#                 {"role": "user", "content": content}
+#             ],
+#             max_tokens=150,
+#             )
+#         output = response.choices[0].message.content
+#         output_lines = output.splitlines()
+#         output_lines = output_lines[1:-1]
+#         output = "\n".join(output_lines)
+#         # print(output)
+#         flight_data = json.loads(output)
+#         return flight_data
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
 
 def extract_works_data():
-    flight_codes = get_airports_from_data()
+    return get_airports_data_from_works_report()
 
 # def extract_text_from_pdf(file_path):
 
@@ -236,28 +238,40 @@ if __name__ == "__main__":
     #         file_path = os.path.join(pdf_directory, filename)
     #         load_emission_data_from_csv('emission_report.csv')
     #         all_emission_data = extract_text_from_pdf(file_path)
-    # load_emission_data_from_csv('emission_report.csv')
+    existing_airports = load_emission_data_from_csv('emission_report.csv')
     # all_emission_data = extract_text_from_pdf("")        
     # create_emission_report(all_emission_data)
-    flight_codes = get_airport_pairs()
-    for trip in flight_codes:
+
+    # Calculating works flight data
+    works_flight_data = extract_works_data()
+
+    # # Calculating the athletics flight data
+    athletics_flight_data = get_airports_from_athletics_data()
+
+    airports_data = []
+    airports_data.extend(works_flight_data)
+    airports_data.extend(athletics_flight_data)
+    # airport_pair = {}
+    # airport_pair["from_airport"] = "IAD"
+    # airport_pair["to_airport"] = "RDU"
+    # airports_data.append(airport_pair)
+    final_emissions_data = []
+
+
+    index = 0
+    for trip in airports_data:
+        print(f"Processing {index + 1} of {len(airports_data)} ({(index + 1) / len(airports_data) * 100:.2f}%)")
+        index += 1
         if (trip['from_airport'],trip['to_airport']) in existing_airports:
-            continue
+            model = existing_airports[(trip['from_airport'],trip['to_airport'])]
+            final_emissions_data.append(model)
         else:
-            response = requests.post(f'https://airportgap.com/api/airports/distance?from={trip["from_airport"]}&to={trip["to_airport"]}')
-            if response.status_code == 200:
-                data = response.json()
-                carbon_emission = calculate_carbon_emission(data['data']['attributes']['miles'])
-                print(carbon_emission)
-                model = EmissionModel(trip['from_airport'],
-                                    trip['to_airport'],
-                                    data['data']['attributes']['from_airport']['latitude'],
-                                    data['data']['attributes']['to_airport']['latitude'],
-                                    data['data']['attributes']['from_airport']['longitude'],  
-                                    data['data']['attributes']['to_airport']['longitude'],
-                                    data['data']['attributes']['miles'],
-                                    carbon_emission)
-                existing_airports[(trip['from_airport'],trip['to_airport'])] = model
+            model = calculate_flight_emissions(trip)
+            existing_airports[(trip['from_airport'],trip['to_airport'])] = model
+            final_emissions_data.append(model)
+
+    # print(final_emissions_data)
+    create_emission_report(final_emissions_data)        
 
 
     
