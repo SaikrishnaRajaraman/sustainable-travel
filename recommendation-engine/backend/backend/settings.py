08b0ad56c48346a76de721d45f0e4c630b564eaa
Saikrishna Rajaraman <saikrishnarajaraman96@gmail.com>
2025-02-17 14:25:28 -0500

    Database url updated

--- recommendation-engine/backend/backend/settings.py
+++ recommendation-engine/backend/backend/settings.py
@@ -94,7 +94,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 database_url = os.environ.get('DATABASE_URL')
 
-DATABASES['default'] = dj_database_url.parse(database_url)
+
+DATABASES = {
+    'default': dj_database_url.parse(database_url)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
