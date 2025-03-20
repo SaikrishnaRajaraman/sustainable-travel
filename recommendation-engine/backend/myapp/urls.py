bcde7a33da48fe8cd71c455988aaaf4fb903301d
Saikrishna Rajaraman <saikrishnarajaraman96@gmail.com>
2025-03-20 19:20:44 -0400

    Fixes

--- recommendation-engine/backend/myapp/urls.py
+++ recommendation-engine/backend/myapp/urls.py
@@ -8,7 +8,7 @@ router.register(r'flights', FlightDataViewSet, basename='flight')
 
 
 urlpatterns = [
-    # path("", views.home, name="home"),
+    path("", views.home, name="home"),
     path('api/', include(router.urls)),
     path('api/query/', views.langchain_query, name='query'),
     path('api/calculatemiles/',views.upload_csv_file, name='calculatemiles')
